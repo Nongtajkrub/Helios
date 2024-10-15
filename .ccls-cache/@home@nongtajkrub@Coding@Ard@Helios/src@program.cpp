9d4a538ffdde9249fbd64f1ebf97c18c837bdf9f
Nongtajkrub <sunanborthwic@gmail.com>
2024-10-15 15:51:38 +0700

    Change how struct program::data work by sepearting data to other structs

--- .ccls-cache/@home@nongtajkrub@Coding@Ard@Helios/src@program.cpp
+++ .ccls-cache/@home@nongtajkrub@Coding@Ard@Helios/src@program.cpp
@@ -11,50 +11,68 @@
 
 #define LOOP_DELAY 50
 
-namespace ui {
+#define UP_BUTT_PIN 12
+#define DOWN_BUTT_PIN 14
+#define SEL_BUTT_PIN 27
 
-}
+#define UI_WELCOM_TXT_ID 0
+#define UI_SETTING_OPT_ID 1
+#define UI_CONTROL_OPT_ID 2
 
 namespace program {
-	static void init_lcd(struct program_data* data) {
-		data->lcd = I2C(LCD_ADDR, LCD_COLS, LCD_ROWS);
-	}
-	
-	static void init_ui(struct program_data* data) {
-		data->welcom_txt = ui::make("Welcome!", ui::TXT);
-		data->setting_opt = ui::make("Settings", ui::OPT);
-		data->ui_group = ui::group(
-				&data->lcd,
+	static void init_ui(struct data* data) {
+		struct ui_data ui = data->ui;
+
+		ui.on_menu = MAIN;
+
+		ui.lcd = new I2C(LCD_ADDR, LCD_COLS, LCD_ROWS);
+		ui.lcd->init();
+
+		ui.main.welcom_txt  = ui::make("Welcome!", ui::TXT);
+		ui.main.setting_opt = ui::make("Settings", ui::OPT);
+		ui.main.control_opt = ui::make("Controls", ui::OPT);
+		ui.main.group       = ui::group(
+				ui.lcd,
 				LCD_ROWS,
 				LCD_COLS,
-				2,
-				data->welcom_txt,
-				data->setting_opt
+				3,
+				ui.main.welcom_txt,
+				ui.main.setting_opt,
+				ui.main.control_opt
 				);
-	}
-	
-	static void init_ldr(struct program_data* data) {
-		data->ldr1 = ldr::make(LDR_PIN);
+
+		ui.up_button   = button::make(UP_BUTT_PIN);
+		ui.down_button = button::make(DOWN_BUTT_PIN);
+		ui.sel_button  = button::make(SEL_BUTT_PIN);
 	}
 
-	static void init_np(struct program_data* data) {
-		data->pixel = np::make(NP_PIN, NP_COUNT);
+	static void init_ldr(struct data* data) {
+		data->ldr.ldr1 = ldr::make(LDR_PIN);
 	}
 
-	void setup(struct program_data* data) {
-		init_lcd(data);
+	static void init_np(struct data* data) {
+		data->np.pixel = np::make(NP_PIN, NP_COUNT);
+	}
+
+	void setup(struct data* data) {
 		init_ui(data);
 		init_ldr(data);
 		init_np(data);
 	}
 
-	static void ui_loop(struct program_data* data) {
-		ui::show(data->ui_group);
+	static void ui_loop(struct data* data) {
+		struct ui_data ui = data->ui;
+
+		switch (ui.on_menu) {
+		case MAIN:
+			ui::show(ui.main.group);
+			break;
+		case SETTING:
+			break;
+		}
 	}
 
-	void loop(struct program_data* data) {
-		delay(LOOP_DELAY);
+	void loop(struct data* data) {
 
-		ui_loop(data);
 	}
-};
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,13 @@
-#define LCD_ADDR 0x27
-#define LCD_COLS 20
-#define LCD_ROWS 4
-
-#include "ui.hpp"
-#include "electronics.hpp"
+#include "program.hpp"
 #include <Arduino.h>
 #include <util/type.hpp>
 
-// ui
-I2C screen(LCD_ADDR, LCD_COLS, LCD_ROWS);
-
-ui::elem_t* elem1;
-ui::elem_t* elem2;
-ui::elem_t* elem3;
-ui::elem_t* elem4;
-ui::elem_t* elem5;
-ui::elem_t* elem6;
-ui::group_t* group;
-
-button::butt_t* butt1;
-button::butt_t* butt2;
-button::butt_t* butt3;
-
-ldr::ldr_t* ldr1;
-
-np::pixel_t* pixel;
+struct program::data program_data;
 
 void setup() {
-	screen.init();
-	elem1 = ui::make("Welcome", ui::TXT);
-	elem2 = ui::make("Opt1", ui::OPT);
-	elem3 = ui::make("Opt2", ui::OPT);
-	elem4 = ui::make("Opt3", ui::OPT);
-	elem5 = ui::make("Opt4", ui::OPT);
-	elem6 = ui::make("Opt5", ui::OPT);
-	group = ui::group(
-			&screen,
-			LCD_ROWS,
-			LCD_COLS,
-			6,
-			elem1,
-			elem2,
-			elem3,
-			elem4,
-			elem5,
-			elem6
-			);
-
-	butt1 = button::make(27);
-	butt2 = button::make(12);
-	butt3 = button::make(14);
-
-	ldr1 = ldr::make(34);
-
-	pixel = np::make(26, 1);
-
-	Serial.begin(9600);
-}
-
-static void handle_select(struct ui::selector_in info) {
-	switch (info.id) {
-	case 0:
-		Serial.println("Invalid");
-		break;
-	case 1:
-		Serial.println(ldr::read(ldr1));
-		break;
-	case 2:
-		Serial.println("On");
-		np::color(pixel, 100, 0, 0);
-		break;
-	case 3:
-		Serial.println("Off");
-		np::color(pixel, 0, 0, 0);
-		break;
-	default:
-		break;
-	}
+	program::setup(&program_data);
 }
 
 void loop() {
-	ui::show(group);
-
-	if (button::state(butt1)) {
-		handle_select(ui::selector_on(group));
-	} else if (button::state(butt2)) {
-		ui::selector_up(group);
-	} else if (button::state(butt3)) {
-		ui::selector_down(group);
-	}
-
-	delay(50);
+	program::loop(&program_data);
 }
